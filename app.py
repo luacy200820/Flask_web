cd109fcde319a76573e37cd7f0a4ae992b67ae19
unknown <luacy200820@example.com>
2023-05-23 03:19:40 +0800

    second modify

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,url_for,request,session,redirect
 import sqlite3 
+from prettytable import PrettyTable
 app = Flask(__name__,static_folder='./templates/')
 app.secret_key = 'itissecectkey'
 @app.route('/')
@@ -24,17 +25,19 @@ def addrec():
             ID = request.form["username"]
             sec = request.form["password"]
 
+
             with sqlite3.connect("database.db") as conn:
                 cur= conn.cursor()
-                cur.execute("INSERT INTO user (ID,num) VALUES (?,?)", (ID,sec))
+                cur.execute("INSERT INTO user (ID,num,eschar,slough,granulation) VALUES (?,?,?,?,?)", (ID,sec,0,0,0))
                 conn.commit()
                 msg = "successfully add"
         except:
             conn.rollback()
             msg = "error in insert"
         finally:
-            return render_template("result.html",msg=msg)
             conn.close()
+            return render_template("result.html",msg=msg)
+            
 
 @app.route('/login',methods = ["POST","GET"])
 def login():
@@ -55,7 +58,9 @@ def login():
             session['id']  = acc['ID']
             session['sec'] = acc['num']
             msg = 'logged in successfully'
-            return render_template('analysis.html',msg=msg)
+            print("acc", acc['ID'])
+            return redirect('/analysis')
+            # return render_template('analysis.html',msg=acc['num'])
         else:
             msg = 'incorrect id / password'
         # msg = rows
@@ -76,8 +81,36 @@ def list():
     cur = conn.cursor()
     cur.execute("select * from user")
     rows = cur.fetchall()
+ 
     return render_template('list.html',rows=rows)
+@app.route('/analysis', methods=['GET', 'POST'])
+def result():
+    selected_date = "NA"
+    if request.method == 'POST':
+        selected_date = request.form['date']
+    value = session['id']   
+    n = 0.6
+    s = 8
+    g = 3
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
 
+    query = "UPDATE user SET eschar =?, slough=?,granulation=? WHERE id = ?"
+    cursor.execute(query,(n,s,g,value))
+    conn.commit()
+    # conn.close()
+    # query = "SELECT * FROM user"
+    # cursor.execute(query)
+    # rows = cursor.fetchall()
+    # for row in rows:
+    #     print(row[0])
+    #     print(row[1])
+    #     print(row[2])
+    # print(rows)
+    cursor.close()
+    conn.close()
+
+    return render_template("analysis.html",value = value,date=selected_date)
 
 if __name__ == '__main__':
    app.run(debug = True)
--- conn_db.py
+++ conn_db.py
@@ -5,7 +5,13 @@ print( "Opened database successfully")
 
 conn.execute('''CREATE TABLE user (
     ID INTEGER PRIMARY KEY,
-    num TEXT NOT NULL
+    num TEXT NOT NULL,
+    visit_time TIMESTAMP,
+    eschar FLOAT,
+    slough FLOAT,
+    granulation FLOAT,
+    area FLOAT,
+    img BLOB
 );''')
 print( "Table created successfully")
 conn.close()
